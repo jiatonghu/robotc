7569f0e5055a350a6da1a0d0a0ded4dfc60c92e4
Ningxin Hu <ningxin.hu@intel.com>
2019-09-11 22:43:09 +0800

    Update the comment of vacuum.c

--- challenge_pack/sensors/vacuum.c
+++ challenge_pack/sensors/vacuum.c
@@ -1,5 +1,10 @@
 
-// Use a Touch Sensor, program the robot to touch all four walls of a room!
+// Using a Touch Sensor, program the robot to touch all four walls of a room!
+// Using a 4'x4 table (with walls), have the robot start near the middle facing straight
+// to a wall. Using a touch sensor, program the robot to move forward and touch ALL four walls.
+// Note: The touch sensor is under the robot's arm, and does not need to reset to the center
+// after each wall.
+
 task main()
 {
 
