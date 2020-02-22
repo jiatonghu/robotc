ca2e7e2e17b55dd90303224f51ddd133f0ae1307
Jiatong Hu <hujiatong2009@outlook.com>
2020-02-22 21:08:31 +0800

    Update Strawberry_Plant_Challenge.c

--- challenge_pack/program_flow/Strawberry_Plant_Challenge.c
+++ challenge_pack/program_flow/Strawberry_Plant_Challenge.c
@@ -28,7 +28,7 @@ task main()
 	while(1){
 		setMultipleMotors(50, rightMotor, leftMotor);
 		while(getDistanceValue(port7) > 100){}
-		stopMultipleMotors(50, rightMotor, leftMotor);
+		stopMultipleMotors(rightMotor, leftMotor);
 		moveMotor(motor11, 2, seconds, 50);
 		moveMotor(motor10, 0.7, seconds, 50);
 		if(getColorName(port3) == colorGreen){
